65676159b348058d205e3bd898f456ac0485c3a8
Isaac <iadzraku@gmail.com>
2024-03-14 18:56:51 +0000

    implement dashboard elements

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -1,7 +1,10 @@
 export default function Dashboard(){
     return (
         <div className="flex justify-center items-center h-screen">
-            <h3>Dashboard</h3>
+            <h3 className="border min-w-0 p-4 rounded mr-4 text-center bg-gray-200">Lecturers</h3>
+            <h3 className="border min-w-0 p-4 rounded mr-4 text-center bg-gray-200">Courses</h3>
+            <h3 className="border min-w-0 p-4 rounded mr-4 text-center bg-gray-200">Halls</h3>
+            
         </div>
     )
 }
